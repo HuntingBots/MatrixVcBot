c264146b722f191ec21aefbf69f11c675b40993b
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-09 23:48:11 +0530

    Create userbotleave.py

---
+++ handlers/userbotleave.py
@@ -0,0 +1,99 @@
+from callsmusic.callsmusic import client as USER
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserAlreadyParticipant
+from helpers.decorators import errors, authorized_users_only
+
+
+@Client.on_message(filters.command(["userbotleave"]) & ~filters.private & ~filters.bot)
+@authorized_users_only
+@errors
+async def addchannel(client, message):
+    chid = message.chat.id
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        await message.reply_text(
+            "<b>Add me as admin of yor group first</b>",
+        )
+        return
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name = "AsunaMusic"
+
+    try:
+        await USER.leave_chat(automatically)
+        await USER.send_message(message.chat.id, "I joined here as you requested")
+    except UserAlreadyParticipant:
+        await message.reply_text(
+            "<b>helper already in your chat</b>",
+        )
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
+            "\n\nOr manually remove @asunahelper to your Group and try again</b>",
+        )
+        return
+    await message.reply_text(
+        "<b>helper userbot leaved your chat</b>",
+    )
+
+
+@USER.on_message(filters.group & filters.command(["userbotleave"]))
+async def rem(USER, message):
+    try:
+        await USER.leave_chat(message.chat.id)
+    except:
+        await message.reply_text(
+            f"<b>User couldn't leave your group! May be floodwaits."
+            "\n\nOr manually kick me from to your Group</b>",
+        )
+        return
+
+@Client.on_message(filters.command(["userbotjoinchannel","ubjoinc"]) & ~filters.private & ~filters.bot)
+@authorized_users_only
+@errors
+async def addcchannel(client, message):
+    try:
+      conchat = await client.get_chat(message.chat.id)
+      conid = conchat.linked_chat.id
+      chid = conid
+    except:
+      await message.reply("Is chat even linked")
+      return    
+    chat_id = chid
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        await message.reply_text(
+            "<b>Add me as admin of yor channel first</b>",
+        )
+        return
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name = "AsunaMusic"
+
+    try:
+        await USER.leave_chat(automatically)
+        await USER.send_message(message.chat.id, "I leave here as you requested")
+    except UserAlreadyParticipant:
+        await message.reply_text(
+            "<b>helper leave in your channel</b>",
+        )
+        return
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
+            "\n\nOr manually leave @asunahelper to your Group and try again</b>",
+        )
+        return
+    await message.reply_text(
+        "<b>helper userbot leaved your channel</b>",
+    )
+    
